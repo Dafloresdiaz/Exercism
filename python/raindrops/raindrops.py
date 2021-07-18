19d16d7dfd3e803abc7af4ec3e8ea5a88837dfa9
dafloresdiaz <dafloresdiaz@gmail.com>
2021-07-17 19:30:04 -0500

    First attempt for raindrops exercise

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,11 @@
 def convert(number):
-    pass
+    rain = ""
+    if number % 3 == 0:
+        rain = rain+"Pling"
+    if number % 5 == 0:
+        rain = rain+"Plang"
+    if number % 7 == 0:
+        rain = rain+"Plong"
+    if not rain:
+        return str(number)
+    return rain
