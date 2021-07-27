c7878427d3b828eb18b40c9d28bcf6960daba8ab
dafloresdiaz <dafloresdiaz@gmail.com>
2021-07-26 23:26:54 -0500

    Solve the highscores exercise

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,10 +1,12 @@
 def latest(scores):
-    pass
+    return scores[-1]
 
 
 def personal_best(scores):
-    pass
+    return max(scores)
 
 
 def personal_top_three(scores):
-    pass
+    scores.sort()
+    scores.reverse()
+    return scores[:3]
