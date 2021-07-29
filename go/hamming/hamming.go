940c9f349e46500566c372181637c3cb5b3a07b1
dafloresdiaz <dafloresdiaz@gmail.com>
2021-07-28 23:44:58 -0500

    Solve the hamming exercise

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,4 +1,17 @@
 package hamming
 
+import "errors"
+
 func Distance(a, b string) (int, error) {
+	differences := 0
+	if len(a) != len(b) {
+		return -1, errors.New("the length is not the same")
+	}
+	for iteration := 0; iteration < len(a); iteration++ {
+		if a[iteration] != b[iteration] {
+			differences += 1
+		}
+	}
+
+	return differences, nil
 }
