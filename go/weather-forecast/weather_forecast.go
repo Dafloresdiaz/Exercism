c5dd7d47354a6b1e6b7ee2d3cbf993eae271535b
Daniel Flores <dafloresdiaz@gmail.com>
2022-06-01 18:20:38 -0500

    Put comments

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather to know the current weather condition.
 package weather
 
+//CurrentCondition to know the current  condition.
 var CurrentCondition string
+
+//CurrentLocation to know the current location.
 var CurrentLocation string
 
+//Forecast return the current location and the current condition.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
